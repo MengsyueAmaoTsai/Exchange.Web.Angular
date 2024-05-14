6139f12566cbd10bd400f45559568acc1c6d7bbc
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-05-14 23:51:42 +0800

    Apply Microsoft Fluent Web Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +6,9 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
-  title = 'richillcapital-exchange-web';
+  title = 'RichillCapital.Exchange.Web';
 }
