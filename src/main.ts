1ac42ac4c2b1b3ebbf489925cb0eccc49c39ccf0
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-05-14 23:40:22 +0800

    Install FluentUI

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import { provideFluentDesignSystem, allComponents } from '@fluentui/web-components';
+
+provideFluentDesignSystem().register(allComponents);
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
