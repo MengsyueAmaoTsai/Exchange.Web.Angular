fdb5610fe6a94cc55a972341fff502869233d4ef
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-07-11 00:47:33 +0800

    add home page and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { AppHeaderComponent } from "./components/app-header/app-header.component";
 
 @Component({
 	selector: "app-root",
 	standalone: true,
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, AppHeaderComponent],
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss",
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from "@angular/router";
-import { AboutComponent } from "./pages/about.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 export const routes: Routes = [
 	{
-		path: "about",
-		component: AboutComponent,
+		path: "",
+		component: HomeComponent,
 	},
 ];
