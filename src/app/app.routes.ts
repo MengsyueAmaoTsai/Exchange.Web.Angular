c97a7e816dd88754b172f0503410105a874a18f6
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-07-11 00:38:50 +0800

    add about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { AboutComponent } from "./pages/about.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: "about",
+		component: AboutComponent,
+	},
+];
---
+++ src/app/pages/about.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "about-page",
+	templateUrl: "./about.component.html",
+	styleUrl: "./about.component.scss",
+})
+export class AboutComponent {
+	title = "About";
+}
