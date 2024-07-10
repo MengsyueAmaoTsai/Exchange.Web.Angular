23a4621d5fc2742aff09240ce1f825a45051b3e4
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-07-11 03:30:54 +0800

    add order entry

---
+++ src/app/components/order-entry/order-entry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "order-entry",
+	standalone: true,
+	imports: [],
+	templateUrl: "./order-entry.component.html",
+	styleUrl: "./order-entry.component.scss",
+})
+export class OrderEntryComponent {}
