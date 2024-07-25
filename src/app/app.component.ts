e7410804ca7f1880db83b931c1074c9fa8cd8d4d
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-07-25 15:36:27 +0800

    Remove all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { AppHeaderComponent } from "./components/app-header/app-header.component";
-import { AppStatusBarComponent } from "./components/app-status-bar/app-status-bar.component";
-import { OrderEntryComponent } from "./components/order-entry/order-entry.component";
 
 @Component({
 	selector: "app-root",
 	standalone: true,
-	imports: [RouterOutlet, AppHeaderComponent, AppStatusBarComponent],
+	imports: [RouterOutlet],
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss",
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { Component, type OnInit, inject } from "@angular/core";
 import { ApiService } from "../../api.service";
-import { OrderEntryComponent } from "../../components/order-entry/order-entry.component";
 
 @Component({
 	selector: "app-home",
 	standalone: true,
-	imports: [CommonModule, OrderEntryComponent],
+	imports: [CommonModule],
 	templateUrl: "./home.component.html",
 	styleUrl: "./home.component.scss",
 })
