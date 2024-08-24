230106b27fe6c66ba48f49c1c2041ad9fec44b51
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-25 07:47:21 +0800

    Add default page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from "@angular/router";
+import { DefaultComponent } from "./pages/default/default.component";
 
 export const routes: Routes = [
-	// {
-	// 	path: "",
-	// 	component: HomeComponent,
-	// },
+	{
+		path: "",
+		component: DefaultComponent,
+	},
 ];
---
+++ src/app/pages/default/default.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "default",
+	standalone: true,
+	templateUrl: "./default.component.html",
+	styleUrls: ["./default.component.scss"],
+})
+export class DefaultComponent {}
