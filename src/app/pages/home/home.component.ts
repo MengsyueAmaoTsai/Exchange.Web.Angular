197449bd9571e59a30caf3cd32db48f334d4e3c7
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-24 10:59:22 +0800

    Add order entry

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
 import { Component, type OnInit, inject } from "@angular/core";
 
 @Component({
@@ -9,7 +10,28 @@ import { Component, type OnInit, inject } from "@angular/core";
 	styleUrl: "./home.component.scss",
 })
 export class HomeComponent implements OnInit {
-	public constructor() {}
+	public symbol = "MAX:USDTTWD";
+	public side = "Buy";
+	public quantity = 1;
+	public price = 32.003;
+
+	public constructor(private httpClient: HttpClient) {}
 
 	public ngOnInit(): void {}
+
+	public placeOrder = async (): Promise<void> => {
+		const response = this.httpClient
+			.post("https://localhost:10000/api/v1/orders", {
+				symbol: this.symbol,
+				side: this.side,
+				type: "LIMIT",
+				timeInForce: "GTC",
+				quantity: this.quantity,
+				price: this.price,
+			})
+			.subscribe((response) => {
+				console.log(response);
+			});
+		console.log(response);
+	};
 }
