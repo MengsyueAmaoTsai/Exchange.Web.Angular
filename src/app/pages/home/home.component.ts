5debeb1f44fa97bd45904d2598b0fa5bfc110c4f
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-24 22:39:24 +0800

    TradingClock

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,37 +1,74 @@
 import { CommonModule } from "@angular/common";
 import { HttpClient } from "@angular/common/http";
-import { Component, type OnInit, inject } from "@angular/core";
+import { Component, type OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { TradingClockComponent } from "../../components/trading-clock.component";
+
+enum PositionSide {
+	Long = "Long",
+	Short = "Short",
+}
+
+enum OrderSide {
+	Buy = "Buy",
+	Sell = "Sell",
+}
+
+type OrderResponse = {
+	id: string;
+	symbol: string;
+	side: OrderSide;
+	quantity: number;
+	price: number;
+	createdTimeUtc: Date;
+};
+
+type ExecutionResponse = {
+	id: string;
+	orderId: string;
+	quantity: number;
+	price: number;
+	executedTimeUtc: Date;
+};
+
+type PositionResponse = {
+	id: string;
+	symbol: string;
+	side: PositionSide;
+	quantity: number;
+	averagePrice: number;
+	createdTimeUtc: Date;
+};
 
 @Component({
 	selector: "app-home",
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, FormsModule],
 	templateUrl: "./home.component.html",
 	styleUrl: "./home.component.scss",
 })
-export class HomeComponent implements OnInit {
-	public symbol = "MAX:USDTTWD";
-	public side = "Buy";
-	public quantity = 1;
-	public price = 32.003;
+export class HomeComponent {
+	public orders: OrderResponse[] = [];
+	public executions: ExecutionResponse[] = [];
+	public positions: PositionResponse[] = [];
 
-	public constructor(private httpClient: HttpClient) {}
-
-	public ngOnInit(): void {}
-
-	public placeOrder = async (): Promise<void> => {
-		const response = this.httpClient
-			.post("https://localhost:10000/api/v1/orders", {
-				symbol: this.symbol,
-				side: this.side,
-				type: "LIMIT",
-				timeInForce: "GTC",
-				quantity: this.quantity,
-				price: this.price,
-			})
-			.subscribe((response) => {
-				console.log(response);
+	public constructor(private httpClient: HttpClient) {
+		this.httpClient
+			.get<OrderResponse[]>("https://localhost:10000/api/v1/orders")
+			.subscribe((orders) => {
+				this.orders = orders;
 			});
-		console.log(response);
-	};
+
+		this.httpClient
+			.get<ExecutionResponse[]>("https://localhost:10000/api/v1/executions")
+			.subscribe((executions) => {
+				this.executions = executions;
+			});
+
+		this.httpClient
+			.get<PositionResponse[]>("https://localhost:10000/api/v1/positions")
+			.subscribe((positions) => {
+				this.positions = positions;
+			});
+	}
 }
