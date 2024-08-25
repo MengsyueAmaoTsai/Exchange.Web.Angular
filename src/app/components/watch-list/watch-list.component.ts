b918297251f4a017e7a898ed0287278df944e447
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-25 15:42:10 +0800

    update watch list

--- src/app/components/watch-list/watch-list.component.ts
+++ src/app/components/watch-list/watch-list.component.ts
@@ -16,16 +16,16 @@ type WatchListItem = {
 export class WatchListComponent {
 	public watchListItems: WatchListItem[] = [
 		{
-			symbol: "AAPL",
-			last: 123.45,
-			change: 1.23,
-			changePercent: 1.0,
+			symbol: "MAX:USDTTWD",
+			last: 32.033,
+			change: -0.05,
+			changePercent: -0.05,
 		},
 		{
-			symbol: "GOOGL",
-			last: 2345.67,
-			change: 12.34,
-			changePercent: 0.5,
+			symbol: "CME:NQ-1",
+			last: 18564,
+			change: 236,
+			changePercent: 0.76,
 		},
 	];
 }
