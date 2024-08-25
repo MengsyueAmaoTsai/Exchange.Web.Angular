a0d280719833c2883b0c9339072f4d039d963d47
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-25 13:25:11 +0800

    Update watch list

---
+++ src/app/components/watch-list/watch-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "watch-list",
+	templateUrl: "./watch-list.component.html",
+	styleUrls: ["./watch-list.component.scss"],
+	standalone: true,
+})
+export class WatchListComponent {}
--- src/app/pages/default/default.component.ts
+++ src/app/pages/default/default.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
+import { WatchListComponent } from "../../components/watch-list/watch-list.component";
 
 @Component({
 	selector: "default",
 	standalone: true,
 	templateUrl: "./default.component.html",
 	styleUrls: ["./default.component.scss"],
+	imports: [WatchListComponent],
 })
 export class DefaultComponent {}
