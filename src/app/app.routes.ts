e1c5c0a3a21098b0f1469ced3e891e58eef6e3cf
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-25 11:18:38 +0800

    Add dev page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from "@angular/router";
 import { DefaultComponent } from "./pages/default/default.component";
+import { DevComponent } from "./pages/dev/dev.component";
 
 export const routes: Routes = [
 	{
 		path: "",
 		component: DefaultComponent,
+		pathMatch: "full",
+	},
+	{
+		path: "dev",
+		component: DevComponent,
 		pathMatch: "full",
 	},
 	{
---
+++ src/app/pages/dev/dev.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "dev",
+	templateUrl: "./dev.component.html",
+	styleUrls: ["./dev.component.scss"],
+	standalone: true,
+})
+export class DevComponent {}
