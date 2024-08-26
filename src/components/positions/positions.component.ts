c1e5635b2ecac31361b9c4238ef9f5583b9d4a76
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-27 05:28:43 +0800

    orders and positions

---
+++ src/components/positions/positions.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "positions",
+	templateUrl: "./positions.component.html",
+	styleUrls: ["./positions.component.scss"],
+	standalone: true,
+})
+export class PositionsComponent {}
