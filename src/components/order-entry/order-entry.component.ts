9d551043bed28874dd97db0c1e706d9d0ebff413
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-27 19:36:52 +0800

    add buy/sell

--- src/components/order-entry/order-entry.component.ts
+++ src/components/order-entry/order-entry.component.ts
@@ -6,4 +6,11 @@ import { Component } from "@angular/core";
 	styleUrls: ["./order-entry.component.scss"],
 	standalone: true,
 })
-export class OrderEntryComponent {}
+export class OrderEntryComponent {
+	public async buy(): Promise<void> {
+		console.log("Buy");
+	}
+	public async sell(): Promise<void> {
+		console.log("Sell");
+	}
+}
