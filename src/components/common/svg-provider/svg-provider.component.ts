1879cb92e8e4c264ffa377815b33039d5c04a377
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-28 21:13:44 +0800

    f

---
+++ src/components/common/svg-provider/svg-provider.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "svg-provider",
+  templateUrl: "./svg-provider.component.html",
+  standalone: true,
+})
+export class SvgProviderComponent {
+}
