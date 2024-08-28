46758be42f37a6d831d394755ec174a0d61caab1
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-28 18:59:25 +0800

    add not implemented components

---
+++ src/components/current-user/current-user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "current-user",
+  templateUrl: "./current-user.component.html",
+  styleUrls: ["./current-user.component.scss"],
+  standalone: true,
+  imports: [],
+})
+export class CurrentUserComponent {}
--- src/components/top-bar/top-bar.component.ts
+++ src/components/top-bar/top-bar.component.ts
@@ -1,12 +1,14 @@
 import { Component } from "@angular/core";
 import { SiteLogoComponent } from "../site-logo/site-logo.component";
 import { SiteBrandComponent } from "../site-brand/site-brand.component";
+import { SystemClockComponent } from "../system-clock/system-clock.component";
+import { CurrentUserComponent } from "../current-user/current-user.component";
 
 @Component({
   selector: "top-bar",
   templateUrl: "./top-bar.component.html",
   styleUrls: ["./top-bar.component.scss"],
   standalone: true,
-  imports: [SiteLogoComponent, SiteBrandComponent],
+  imports: [SiteLogoComponent, SiteBrandComponent, SystemClockComponent, CurrentUserComponent],
 })
 export class TopBarComponent {}
