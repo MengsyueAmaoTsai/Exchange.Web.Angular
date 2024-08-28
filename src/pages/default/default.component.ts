8ff553448cb8529944768bc91c894154729eb912
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-29 04:06:42 +0800

    add trading components

--- src/pages/default/default.component.ts
+++ src/pages/default/default.component.ts
@@ -1,11 +1,13 @@
 import { Component } from "@angular/core";
 import { TradingViewChartComponent } from "../../components/trading-view-chart/trading-view-chart.component";
+import { WatchListComponent } from "../../components/watch-list/watch-list.component";
+import { OrderEntryComponent } from "../../components/order-entry/order-entry.component";
 
 @Component({
 	selector: "default",
 	standalone: true,
 	templateUrl: "./default.component.html",
 	styleUrls: ["./default.component.scss"],
- imports: [TradingViewChartComponent],
+ imports: [TradingViewChartComponent, WatchListComponent, OrderEntryComponent],
 })
 export class DefaultComponent {}
