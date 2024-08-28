2a53ab9bc19d1f931c4b19a9f1aa831512a146a3
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-28 22:46:29 +0800

    TopBar buttons

--- src/exchange.component.ts
+++ src/exchange.component.ts
@@ -1,12 +1,13 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { TopBarComponent } from "./components/top-bar/top-bar.component";
+import { SvgProviderComponent } from "./svg-provider.component";
 
 @Component({
 	selector: "exchange",
 	templateUrl: "./exchange.component.html",
 	styleUrls: ["./exchange.component.scss"],
 	standalone: true,
-	imports: [RouterOutlet, TopBarComponent],
+	imports: [RouterOutlet, SvgProviderComponent, TopBarComponent],
 })
 export class ExchangeComponent {}
---
+++ src/svg-provider.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "svg-provider",
+	templateUrl: "./svg-provider.component.html",
+	standalone: true,
+})
+export class SvgProviderComponent {}
