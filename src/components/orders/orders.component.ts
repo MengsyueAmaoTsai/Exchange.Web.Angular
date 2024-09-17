ac5bc1ce9981e1099e9609e80538cbd7c33d521e
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-18 00:04:03 +0800

    list orders

--- src/components/orders/orders.component.ts
+++ src/components/orders/orders.component.ts
@@ -1,11 +1,35 @@
-import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import type { OnDestroy, OnInit } from "@angular/core";
+import { Component, inject } from "@angular/core";
 
+type OrderResponse = {
+	id: string;
+	accountId: string;
+	symbol: string;
+	tradeType: string;
+	type: string;
+	timeInForce: string;
+	quantity: number;
+	createdTimeUtc: Date;
+};
 @Component({
 	selector: "orders",
 	templateUrl: "./orders.component.html",
 	styleUrls: ["./orders.component.scss"],
 	standalone: true,
 })
-export class OrdersComponent {
-	orders = [];
+export class OrdersComponent implements OnInit, OnDestroy {
+	private readonly httpClient = inject(HttpClient);
+
+	public orders: OrderResponse[] = [];
+
+	public ngOnDestroy(): void {}
+
+	public ngOnInit(): void {
+		this.httpClient
+			.get<OrderResponse[]>("https://localhost:10000/api/v1/orders")
+			.subscribe((orders) => {
+				this.orders = orders;
+			});
+	}
 }
--- src/pages/default/default.component.ts
+++ src/pages/default/default.component.ts
@@ -3,12 +3,13 @@ import { TradingViewChartComponent } from "../../components/trading-view-chart/t
 import { WatchListComponent } from "../../components/watch-list/watch-list.component";
 import { OrderEntryComponent } from "../../components/order-entry/order-entry.component";
 import { ExecutionsComponent } from "../../components/executions/executions.component";
+import { OrdersComponent } from "../../components/orders/orders.component";
 
 @Component({
 	selector: "default",
 	standalone: true,
 	templateUrl: "./default.component.html",
 	styleUrls: ["./default.component.scss"],
- imports: [TradingViewChartComponent, WatchListComponent, OrderEntryComponent, ExecutionsComponent],
+ imports: [TradingViewChartComponent, WatchListComponent, OrderEntryComponent, ExecutionsComponent, OrdersComponent],
 })
 export class DefaultComponent {}
