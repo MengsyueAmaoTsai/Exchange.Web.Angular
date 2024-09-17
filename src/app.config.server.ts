f6b885455d42890056fab24cf547770b3d8c3e72
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-17 20:20:01 +0800

    Add withFetch() in ssr

--- src/app.config.server.ts
+++ src/app.config.server.ts
@@ -1,10 +1,10 @@
-import { provideHttpClient } from "@angular/common/http";
+import { provideHttpClient, withFetch } from "@angular/common/http";
 import { type ApplicationConfig, mergeApplicationConfig } from "@angular/core";
 import { provideServerRendering } from "@angular/platform-server";
 import { appConfig } from "./app.config";
 
 const serverConfig: ApplicationConfig = {
-	providers: [provideServerRendering(), provideHttpClient()],
+	providers: [provideServerRendering(), provideHttpClient(withFetch())],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
