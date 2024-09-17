c4396a804fde54c99c48fce78fb645cdba2d596a
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-17 20:12:09 +0800

    Place order

--- src/components/order-entry/order-entry.component.ts
+++ src/components/order-entry/order-entry.component.ts
@@ -1,5 +1,19 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, inject } from "@angular/core";
+import { OrderType, TimeInForce, TradeType } from "../../domain";
+
+type CreateOrderRequest = {
+	accountId: string;
+	symbol: string;
+	tradeType: TradeType;
+	orderType: OrderType;
+	timeInForce: TimeInForce;
+	quantity: number;
+};
+
+type OrderCreatedResponse = {
+	id: string;
+};
 
 @Component({
 	selector: "order-entry",
@@ -8,13 +22,42 @@ import { Component, inject } from "@angular/core";
 	standalone: true,
 })
 export class OrderEntryComponent {
-	// private readonly httpClient = inject(HttpClient);
+	private readonly httpClient = inject(HttpClient);
 
 	public buy() {
-		console.log("Buy");
+		this.createOrder({
+			accountId: "1",
+			symbol: "AAPL",
+			tradeType: TradeType.Buy,
+			orderType: OrderType.Market,
+			timeInForce: TimeInForce.FillOrKill,
+			quantity: 1,
+		});
 	}
 
 	public sell() {
-		console.log("Sell");
+		this.createOrder({
+			accountId: "1",
+			symbol: "AAPL",
+			tradeType: TradeType.Sell,
+			orderType: OrderType.Market,
+			timeInForce: TimeInForce.FillOrKill,
+			quantity: 1,
+		});
+	}
+
+	private createOrder(createOrderRequest: CreateOrderRequest) {
+		console.log("Create order request:", createOrderRequest);
+
+		return this.httpClient
+			.post<OrderCreatedResponse>(
+				"https://localhost:10000/api/v1/orders",
+				createOrderRequest,
+			)
+			.subscribe((response) => {
+				const { id } = response;
+
+				alert(`Order ${id} has been created`);
+			});
 	}
 }
