1a6b35efcba6aa85721ef48058b7e26805843f39
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-09-19 11:50:37 +0800

    Add logger

--- src/exchange.component.ts
+++ src/exchange.component.ts
@@ -1,9 +1,11 @@
 import { isPlatformServer } from "@angular/common";
 import { Component, PLATFORM_ID, inject } from "@angular/core";
-import type { OnInit } from "@angular/core";
+import type { InjectionToken, OnInit } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { StatusBarComponent } from "./components/status-bar/status-bar.component";
 import { TopBarComponent } from "./components/top-bar/top-bar.component";
+import { DefaultLogger } from "./infrastructure/logging";
+import type { ILogger } from "./infrastructure/logging";
 import { SvgProviderComponent } from "./svg-provider.component";
 
 @Component({
@@ -17,17 +19,31 @@ import { SvgProviderComponent } from "./svg-provider.component";
 		TopBarComponent,
 		StatusBarComponent,
 	],
+	providers: [
+		{
+			provide: "ILogger<ExchangeComponent>",
+			useFactory: () => new DefaultLogger<ExchangeComponent>(ExchangeComponent),
+		},
+	],
 })
 export class ExchangeComponent implements OnInit {
+	public logger = inject(
+		"ILogger<ExchangeComponent>" as unknown as InjectionToken<
+			ILogger<ExchangeComponent>
+		>,
+	);
+
 	public platformId = inject(PLATFORM_ID);
 
 	public ngOnInit(): void {
-		console.log("ExchangeComponent initialized");
+		this.logger.logDebug("ExchangeComponent initialized");
 
 		if (isPlatformServer(this.platformId)) {
-			console.log("[Exchange:OnInit:Server] Running on the server");
+			this.logger.logWarning("[Exchange:OnInit:Server] Running on the server");
 		} else {
-			console.log("[Exchange:OnInit:Client] Running on the client");
+			this.logger.logInformation(
+				"[Exchange:OnInit:Client] Running on the client",
+			);
 		}
 	}
 }
---
+++ src/infrastructure/logging/index.ts
@@ -0,0 +1,48 @@
+export interface ILogger<T> {
+	logInformation(message: string): void;
+	logError(message: string): void;
+	logDebug(message: string): void;
+	logWarning(message: string): void;
+}
+
+export class DefaultLogger<T> implements ILogger<T> {
+	public constructor(private readonly typeT: new (...args: any[]) => T) {}
+
+	public logError(message: string): void {
+		this.log("ERR", message, console.error);
+	}
+
+	public logDebug(message: string): void {
+		this.log("DBG", message, console.debug);
+	}
+
+	public logWarning(message: string): void {
+		this.log("WRN", message, console.warn);
+	}
+
+	public logInformation(message: string): void {
+		this.log("INF", message, console.info);
+	}
+
+	private log(
+		level: string,
+		message: string,
+		handleLog: (message: string) => void,
+	): void {
+		const logMessage = `[${this.getCurrentTime()} ${level}] ${this.typeT.name.replace(
+			"_",
+			"",
+		)} - ${message}`;
+
+		handleLog(logMessage);
+	}
+
+	private getCurrentTime(): string {
+		return new Date().toLocaleTimeString([], {
+			hour: "2-digit",
+			minute: "2-digit",
+			second: "2-digit",
+			hour12: false,
+		});
+	}
+}
